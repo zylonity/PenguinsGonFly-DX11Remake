caf26d89d493ae23810bd120282c2250a60711a7
zylo_dev <70657892+zylonity@users.noreply.github.com>
2024-04-09 14:57:29 +0100

    Made initial scrips

---
+++ Sprites/LuaHelper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <map>
+
+extern "C"
+{
+	#include "../lua-5.4.4/include/lua.h"
+	#include "../lua-5.4.4/include/lauxlib.h"
+	#include "../lua-5.4.4/include/lualib.h"
+}
+
+#ifdef _WIN32
+#pragma comment(lib, "../lua-5.4.4/liblua54.a")
+#endif
